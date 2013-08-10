03f6c8707ae251190e08717a0699aab8563e768e
KatyaRichards <katyarichards42@gmail.com>
2013-08-10 10:21:58 +0100

    Uses Resources

--- Unity Project/New YRS/Assets/Scripts/CreatePeople.cs
+++ Unity Project/New YRS/Assets/Scripts/CreatePeople.cs
@@ -3,16 +3,31 @@ using System.Collections;
 using System.Xml.Serialization;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 public class CreatePeople : MonoBehaviour {
 	public World world;
 	public static Dictionary<Region,Rect> locations = new Dictionary<Region, Rect>();
 	List<Person> People = new List<Person>();
+	public static Total pop = new Total();
 	static int numberofpeople = 500;
 	// Use this for initialization
 	void Start () {
-		using(Stream s = new FileStream("Assets//data.xml",FileMode.Open))
+        TextAsset dataasset = Resources.Load("data") as TextAsset;
+        TextAsset popasset = Resources.Load("Population") as TextAsset;
+        byte[] databytes = Encoding.ASCII.GetBytes(dataasset.text);
+        byte[] popbytes = Encoding.ASCII.GetBytes(popasset.text); 
+        
+		//using(Stream s = new FileStream("Assets//data.xml",FileMode.Open))
+		//	world = (World)new XmlSerializer(typeof(World)).Deserialize(s);
+		//using(Stream s = new FileStream("Assets//Population.xml",FileMode.Open))
+		//	pop = (Total)new XmlSerializer(typeof(Total)).Deserialize(s);
+		
+        using(Stream s = new MemoryStream(databytes))
 			world = (World)new XmlSerializer(typeof(World)).Deserialize(s);
-		List<Rect> regionrects  = new List<Rect>(){new Rect(-10,-20,35,40),new Rect(-100,-40,90,85),new Rect(35,-5,65,55),new Rect(8,15,17,10),new Rect(-10,20,40,30),new Rect(30,-30,70,40)};
+		using(Stream s = new MemoryStream(popbytes))
+			pop = (Total)new XmlSerializer(typeof(Total)).Deserialize(s);
+        
+        List<Rect> regionrects  = new List<Rect>(){new Rect(-10,-20,35,40),new Rect(-100,-35,90,80),new Rect(35,-5,65,55),new Rect(8,15,17,10),new Rect(-10,20,40,30),new Rect(30,-30,70,40)};
 		for(int i = 0;i<world.regions.Count;i++)
 		{
 			
@@ -63,7 +78,7 @@ public class CreatePeople : MonoBehaviour {
 			float number = r.population*proportion;
 			
 			for(int i = 0;i<number;i++)
-				People.Add(Person.Create(r,regioncolour));
+				People.Add(Person.Create(r,regioncolour,index));
 			
 			
 			
@@ -77,16 +92,16 @@ public class CreatePeople : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	public int year = 0;
+	public int Deaths = 0;
 	public int updatetimer = 0;
 	void Update () {
-		if(updatetimer==100)
+		if(updatetimer==50)
 		{
 			updatetimer = 0;
-			year++;
-			((GUIText)(GUIText.FindObjectOfType(typeof(GUIText)))).text = "Year: "+year.ToString(); 
+			((GUIText)(GUIText.FindObjectOfType(typeof(GUIText)))).text = "Death: "+Deaths.ToString(); 
 			foreach(Person p in People)
-					p.Update();
+					if(p.Update())
+                    Deaths++;
 		}
 		else
 			updatetimer++;
@@ -120,24 +135,26 @@ public class Person
 	Region region;
 	int age = 0;
 	bool alive = true;
+    int RegionIndex;
 	
 	public Person(GameObject g,Region r)
 	{
 		gameobject = g;
 		region = r;
 	}
-	public static Person Create(Region r,Color regioncolour)
+	public static Person Create(Region r,Color regioncolour,int regionindex)
 	{
 		
 		
-		RaycastHit hit = new RaycastHit();
+		
+		var hit = new RaycastHit();
 		float x = 0,z = 0;
 		x = Random.Range(CreatePeople.locations[r].x,CreatePeople.locations[r].width+CreatePeople.locations[r].x);
 		z = Random.Range(CreatePeople.locations[r].y,CreatePeople.locations[r].height+CreatePeople.locations[r].y);
 		
 		Ray ray = new Ray(new Vector3(x,50,z),Vector3.down);
 		bool collision = GameObject.Find("ReliefMap").collider.Raycast(ray,out hit,100);
-		while(!(hit.point.y>0 && collision && hit.point!=new Vector3()) )
+		while(!(hit.point.y>0 && collision && hit.point!=new Vector3()))
 		{
 			
 			
@@ -152,10 +169,10 @@ public class Person
 		GameObject g = (GameObject)GameObject.Instantiate(GameObject.Find("Flame"),hitpoint,new Quaternion(0,0,0,0));
 		Person toreturn = new Person(g,r);
 		toreturn.gameobject.transform.FindChild("P").particleSystem.startColor = regioncolour;
-		return new Person(g,r);
+		toreturn.RegionIndex = regionindex;
+        return toreturn;
 	}
-
-	public void Update()
+	public bool Update()
 	{
 		
 		if(alive)
@@ -167,8 +184,7 @@ public class Person
 					agerange=i;
 			
 			int death = 0;
-			//TODO add real population 
-			int randomdeath = Random.Range(0,100000000);
+			float randomdeath = Random.Range(0,CreatePeople.pop.stat[RegionIndex][agerange]*1000);
 			float max = region.stats[agerange].number[region.stats[agerange].number.Count-1];
 			if(randomdeath<max)
 			{
@@ -185,17 +201,26 @@ public class Person
 				Object.Destroy(gameobject);
 				
 				alive = false;
+                return true;;
 			
 			}
 			else
 			{
 				
-			age++;	
+			age++;
+            return false;;
 				
 			}
 				
 		}
+        return false;;
 	}
 				
+	
+}
+public class Total
+{
+	
+	public List<List<float>> stat = new List<List<float>>();
 	
 }
